10e785a75152eea1b42f9ee99d609ce691d015d0
ayeshakhan-29 <aarkk.29@gmail.com>
2023-11-26 05:12:33 +0500

    adjusted navigation

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Home from './src/pages/Home';
 import SurveyPage from './src/pages/SurveyPage';
 import Login from './src/pages/Login';
 import SignupScreen from './src/pages/Signup';
-import FirstPage from './src/pages/FirstPage'
+// import FirstPage from './src/pages/FirstPage'
 import LogoutScreen from './src/pages/Logout';
 // import ToastManager, { Toast } from 'toastify-react-native'
 import Toast from 'react-native-toast-message';
@@ -16,14 +16,14 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="LogoutScreen">
+      <Stack.Navigator initialRouteName="SignupPage">
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen
           name="SurveyPage" 
           component={SurveyPage}        />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="SignupPage" component={SignupScreen} />
-        <Stack.Screen name="FirstPage" component={FirstPage} />
+        {/* <Stack.Screen name="FirstPage" component={FirstPage} /> */}
         <Stack.Screen name="LogoutScreen" component={LogoutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
